aac01cb27d976b57f5b0d2f228949b752500d568
nangonz <nangonz91@gmail.com>
2022-10-13 18:13:45 -0300

    feat(client/src/index.js): Import config dotenv axios

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+axios.defaults.baseURL = process.env.REACT_APP_API;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
