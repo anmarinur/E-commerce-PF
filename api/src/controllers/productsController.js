e84d537c70d5e1fc32aa6cc4680a6111c8377417
nangonz <nangonz91@gmail.com>
2022-10-15 16:15:32 -0300

    fix(api/src/controllers/productsController.js): remove mocktest api-db

--- api/src/controllers/productsController.js
+++ api/src/controllers/productsController.js
@@ -1,17 +1,6 @@
 const { Product } = require('../db.js');
 
 const getProducts = async (req, res) => {
-    for(let i=0; i<25; i++){
-        await Product.create({
-            name: "ThinkPad E14",
-            image: "https://www.lenovo.com/medias/lenovo-laptop-thinkpad-e14-gen-2-hero.png?context=bWFzdGVyfHJvb3R8MjY4OTA2fGltYWdlL3BuZ3xoYTAvaGIyLzE0MTA2OTIyMzE5OTAyLnBuZ3xkMTM5OTVhY2M3ODRhNGZlOGZiN2M0N2RlNjJiYTA4Zjg2ZjUwM2RhY2UyM2VkZWYwMzY4OTA3ZmQ3ZTlkYjVh",
-            description: "Procesamiento hasta Intel® Core™ i7 de 11va generación",
-            price: 289999.00,
-            brand:"Lenovo",
-            stock: 5,
-            category: "laptops"
-        });
-    }
 
     const pageNumber = Number.parseInt(req.query.page);
     const sizeNumber = Number.parseInt(req.query.size);
