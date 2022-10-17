9633b8443d91b54cf0d89538e29ca4cbe5e89506
emuller1996 <stefano.muller@hotmail.com>
2022-10-17 00:11:16 -0500

    fix(client/Nav) add styles and fix error the console browser

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -5,34 +5,37 @@ import orderIcon from "./images/orderIcon.png"
 import logo from "./images/Logo.png"
 import { useAuth0 } from "@auth0/auth0-react";
 import { Profile } from "../LogProfile/Profile";
+import { Link } from 'react-router-dom';
+
+
 
 export default function Nav(){
     const { isAuthenticated } = useAuth0();
 
     return(
-        <nav class="navbar d-inline">
-                <div class="row mx-0 py-2 d-flex justify-content-between align-items-center" style={{"background-color": "#a52323"}}>
-                    <div class="col-2 text-center text-white">+57 3184612011</div>
-                    <a class="col-2 text-center text-white" href="http://localhost:3000">www.tecnoshop.com.ar</a>
+        <nav className="navbar d-inline">
+                <div className="row mx-0 py-2 d-flex justify-content-between align-items-center " style={{backgroundColor: "#a52323"}}>
+                    <div className="col-2 text-center text-white fw-semibold">+57 3184612011</div>
+                    <Link className="col-2 text-center text-white fw-semibold" to="/">www.tecnoshop.com.ar</Link>
                         {isAuthenticated ? <Profile/> : <LoginButton/> }
                 </div>
-                <div class="row mx-0 py-0 align-items-center justify-content-center" style={{"background-color": "#4b1b1b"}}>   
-                    <a class="navbar-brand col-3 text-white" href="http://localhost:3000">
-                    <img src={logo} alt="logo" class="w-25"></img>
+                <div className="row mx-0 py-0 align-items-center justify-content-center" style={{backgroundColor: "#4b1b1b"}}>   
+                    <Link className="navbar-brand col-3 text-white fw-semibold" to="/">
+                    <img src={logo} alt="logo" className="w-25"></img>
                     TECNOSHOP
-                    </a>
-                    <form class="d-flex col-4 my-5" role="search">
-                        <input class="form-control me-0 rounded-end rounded-pill" style={{"background-color": "#d9d9d9"}} type="search" placeholder="Buscar" aria-label="Search"/>
-                        <button class="btn btn-outline-success text-white rounded-start rounded-pill border-0 px-5" style={{"background-color": "#ff3030"}} type="submit">Buscar</button>
+                    </Link>
+                    <form className="d-flex col-4 my-5" role="search">
+                        <input className="form-control me-0 rounded-end rounded-pill bg-light"  type="search" placeholder="Buscar" aria-label="Search"/>
+                        <button className="btn  text-white rounded-start rounded-pill border-0 px-5 btn-danger"  type="submit">Buscar</button>
                     </form>
-                    <div class="d-flex col-4 align-items-center text-center">                    
-                        <div class="text-white mx-5 col">
-                            <img src={favoriteIcon} alt="favoriteIcon" class="w-25 col"></img>
-                            <p class="col">Favoritos</p>
+                    <div className="d-flex col-4 align-items-center text-center">                    
+                        <div className="text-white mx-5 col">
+                            <img src={favoriteIcon} alt="favoriteIcon" className="w-25 col"></img>
+                            <p className="col">Favoritos</p>
                         </div>
-                        <div class="text-white mx-5 col">
-                        <img src={orderIcon} alt="orderIcon" class="w-25 col"/>
-                        <p class="col">Carrito</p>
+                        <div className="text-white mx-5 col">
+                        <img src={orderIcon} alt="orderIcon" className="w-25 col"/>
+                        <p className="col">Carrito</p>
                         </div>
                     </div>
                 </div>
