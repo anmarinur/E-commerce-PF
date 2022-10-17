b61d79a059bc91f7f4e4c46f0260225e127174cd
emuller1996 <stefano.muller@hotmail.com>
2022-10-17 00:03:52 -0500

    feat(client/LogProfile) add style login

--- client/src/components/LogProfile/Logout.jsx
+++ client/src/components/LogProfile/Logout.jsx
@@ -5,6 +5,6 @@ export const LogoutButton = () => {
     const { logout } = useAuth0();
 
     return(
-        <button class="dropdown-item btn btn-primary w-75 ms-2 text-white text-center" style={{"background-color": "#a52323"}} onClick={() => logout({returnTo: window.location.origin}) }>Logout</button>
+        <button className="btn btn-sm btn-danger w-75 ms-2 text-white text-center"  onClick={() => logout({returnTo: window.location.origin}) }> <i className="fa-solid fa-right-from-bracket"></i> Logout</button>
     )
 }
