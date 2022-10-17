c7cd610b12c5fb1f49de3645ccbc05b730f21ede
emuller1996 <stefano.muller@hotmail.com>
2022-10-16 21:21:24 -0500

    feat(client/redux/actions) add sort by order the price

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -6,14 +6,15 @@ export const GET_DETAILS = "GET_DETAILS";
 
 
 
-export const getAllProducts = (size, page, filterCategory) => {
-
-    if (filterCategory) {
-        var catFilter = `&cat=${filterCategory}`;
-    }
+export const getAllProducts = (size, page, filterCategory,sort) => {
+    var catFilter = '';
+    var sortPrice ='';
+    if (filterCategory) catFilter = `&cat=${filterCategory}`;
+    if (sort)  sortPrice = `&ordprice=${sort}`;
+    
     return async function (dispatch) {
         try {
-            const result = await axios.get(`/product?size=${size}&page=${page}${catFilter}`);
+            const result = await axios.get(`/product?size=${size}&page=${page}${catFilter}${sortPrice}`);
             return dispatch({ type: GET_PRODUCTS, payload: result.data });
         } catch (error) {
             console.log(error);
