b34722e5a3db83ec88083288c6fd739e1b0d8e8d
AnaGonzalvez <anaa.gonzalvez2@gmail.com>
2022-10-17 12:11:48 -0300

    feat(/api/productsInfo): smartwatches added

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -2,51 +2,54 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
 
-  sequelize.define('Product', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    }, 
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
+  sequelize.define(
+    "Product",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        validate: {
+          isUrl: true,
+        },
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+          min: 0.01,
+        },
+      },
+      category: {
+        type: DataTypes.ENUM("laptops", "smartphones", "tablets", "smartwatches"),
+        allowNull: false,
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      brand: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    image: {
-      type: DataTypes.STRING,
-      validate: {
-        isUrl: true
-      }
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      }
-    },
-    price: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-      validate: {
-        min: 0.01,
-      }
-    },
-    category: {
-      type: DataTypes.ENUM("laptops","smartphones","tablets"),
-      allowNull: false,
-    },
-    stock: {
-     type: DataTypes.INTEGER,
-     allowNull: false,
-    },
-    brand: {
-     type: DataTypes.STRING,
-     allowNull: false,
+    {
+      timestamps: false,
     }
-  },
-  {
-    timestamps: false
-  });
+  );
 
 }
