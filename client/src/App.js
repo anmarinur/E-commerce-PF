babc55d17432e774ad50c4b7018e2b3e95d86c7c
nahuel <puignahuel.ventas@gmail.com>
2022-10-17 22:08:37 -0300

    feat(client/about) create about page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import PageNotFound from './pages/PageNotFound';
 import FormCreate from './pages/FormCreate';
 import ProductDetail from './pages/ProductDetail';
 import Home from './pages/Home';
+import About from './pages/About'
 import * as bootstrap from 'bootstrap'; // do not delete this line - is for the navBar !!
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
@@ -18,9 +19,13 @@ export default function App() {
           <FormCreate />
         </Route>
         <Route exact path="/product/:id" component={ProductDetail}></Route>
+        <Route exact path="/about">
+          <About/>
+        </Route>
         <Route>
           <PageNotFound />         
         </Route>
+        
       </Switch>      
     </div>
   );
