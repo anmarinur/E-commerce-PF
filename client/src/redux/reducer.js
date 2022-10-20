d4076064a9054f04d2c6cf213646fbec12741ef0
emuller1996 <stefano.muller@hotmail.com>
2022-10-20 13:37:20 -0500

    implement cart and clean code

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,48 +1,80 @@
-import { GET_PRODUCTS, GET_DETAILS, DELETE_PRODUCT, FLAG_UPDATE, ALERT } from "./actions";
+import { GET_PRODUCTS, GET_DETAILS, DELETE_PRODUCT, ADD_TO_CART , DELETE_FROM_CART, ADJUST_QUANTITY, LOAD_CURRENT_ITEM } from "./actions";
 
 const stateInitial = {
     products: {},
     details: {},
-    flagUpdate: {},
-    AlertInfo : { show:false },
+    cart: [],
+    currentItem: null,
 };
 
-export default function rootReducer(state= stateInitial, action){
-    switch(action.type){
-        case GET_PRODUCTS :
-         return {
-             ...JSON.parse(JSON.stringify(state)),
-             products: action.payload
-         }
-        case GET_DETAILS :
-         return {
-          ...JSON.parse(JSON.stringify(state)),
-          details: action.payload
-        }
+export default function rootReducer(state = stateInitial, action) {
+    switch (action.type) {
+        case GET_PRODUCTS:
+            return {
+                ...JSON.parse(JSON.stringify(state)),
+                products: action.payload
+            }
+        case GET_DETAILS:
+            return {
+                ...JSON.parse(JSON.stringify(state)),
+                details: action.payload
+            }
         case DELETE_PRODUCT:
             return {
                 ...JSON.parse(JSON.stringify(state)),
                 products: state.products.filter((p) => p.id !== action.payload),
             }
-        case FLAG_UPDATE:
+        case ADD_TO_CART:
+            //get the item from products array
+            //const products = action.payload.products;
+            const item = state.products.products.find(
+                (prod) => prod.id === action.payload.id
+            );
+            //
+
+            //console.log(state.cart.item, item.maxQuantity);
+            const checkQty = state.cart.find(
+                (item) => item.qty === action.payload.item.maxQuantity
+            );
+            //console.log(checkQty, 123);
+            //check if the item is in cart already
+            const inCart = state.cart.find((item) =>
+                item.id === action.payload.id ? true : false
+            );
             return {
-                ...JSON.parse(JSON.stringify(state)),
-                flagUpdate: {
-                    flag: action.payload.flag,
-                    id: action.payload.id
-                }
-            }
-        case ALERT :
+                ...state,
+                cart: inCart
+                    ? state.cart.map((item) =>
+                        item.id === action.payload.id
+                            ? { ...item, qty: item.qty + 1 }
+                            : item
+                    )
+                    : [...state.cart, { ...item, qty: 1 }],
+            };
+        case DELETE_FROM_CART:
             return {
-                ...JSON.parse(JSON.stringify(state)),
-                AlertInfo: {
-                    type: action.payload.type,
-                    show: action.payload.show,
-                    title: action.payload.title,
-                    body: action.payload.body,
-                }
-            }
-        default: 
-         return state;
+                ...state,
+                cart: state.cart.filter((item) => item.id !== action.payload.id),
+            };
+        case ADJUST_QUANTITY:
+            return {
+                ...state,
+                cart: state.cart.map((item) =>
+                    item.id === action.payload.id
+                        ? { ...item, qty: action.payload.qty }
+                        : item
+                ),
+                currentItem:
+                    state.currentItem.id === action.payload.id
+                        ? { ...state.currentItem, qty: action.payload.qty }
+                        : { ...state.currentItem, qty: 1 },
+            };
+        case LOAD_CURRENT_ITEM:
+            return {
+                ...state,
+                currentItem: action.payload,
+            };
+        default:
+            return state;
     }
 }
