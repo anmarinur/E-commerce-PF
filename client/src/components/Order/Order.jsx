2092c2d9530991b12ec15e04f244943a58e56dd7
emuller1996 <stefano.muller@hotmail.com>
2022-10-21 12:20:40 -0500

    feat(client/Order) add valitation and create component orderdetail

--- client/src/components/Order/Order.jsx
+++ client/src/components/Order/Order.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Footer from '../Footer/Footer';
 import Nav from '../Nav/Nav';
+import FormOrder from './FormOrder';
+import OrderDetailsProduct from './OrderDetailsProduct';
 
 const Order = () => {
+
+    const productsCart = useSelector( state => state.cart);
 
 
     return (
@@ -13,40 +18,20 @@ const Order = () => {
 
             <div className="container mt-4 " >
                 <h2>Order</h2>
-                <div className="row">
-                    <div className="col-7 border rounded bg-light shadow me-4 p-4">
-                        <div class="accordion" id="accordionExample">
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="headingOne">
-                                    <button class="accordion-button text-dark     fw-bold " type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                <div className="row g-4">
+                    <div className="col-xl-7 col-md-8  border border-secondary   rounded bg-light shadow me-4 p-4">
+                        <div className="accordion" id="accordionExample">
+                            <div className="accordion-item">
+                                <h2 className="accordion-header" id="headingOne">
+                                    <button className="accordion-button text-dark     fw-bold " type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                                         Shipping
                                     </button>
                                 </h2>
-                                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                                    <div class="accordion-body">
+                                <div id="collapseOne" className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                    <div className="accordion-body">
 
-                                        <div class="form-floating mb-3">
-                                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
-                                            <label for="floatingInput">Email</label>
-                                        </div>
-                                        <div class="form-floating mb-3">
-                                            <input type="text" class="form-control" id="floatingPassword" placeholder="Password" />
-                                            <label for="floatingPassword">Contact Name</label>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-6">
-                                                <div class="form-floating ">
-                                                    <input type="text" class="form-control" id="floatingPassword" placeholder="Password" />
-                                                    <label for="floatingPassword">City</label>
-                                                </div>
-                                            </div>
-                                            <div className="col-6">
-                                                <div class="form-floating ">
-                                                    <input type="text" class="form-control" id="floatingPassword" placeholder="Password" />
-                                                    <label for="floatingPassword">Department or Region</label>
-                                                </div>
-                                            </div>
-                                        </div>
+                                        <FormOrder />
+
                                     </div>
                                 </div>
                             </div>
@@ -57,9 +42,9 @@ const Order = () => {
                         <div className="row">
                             <div className="col">
                                 <div className="border rounded mt-4 py-4 px-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckIndeterminate" />
-                                        <label class="form-check-label fs-6 fw-bold" for="flexCheckIndeterminate">
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="checkbox" value="" id="flexCheckIndeterminate" />
+                                        <label className="form-check-label fs-6 fw-bold" for="flexCheckIndeterminate">
                                             Agreement with the seller.
                                         </label>
                                     </div>
@@ -67,29 +52,12 @@ const Order = () => {
                             </div>
 
                             <div className="col-12 mt-4 text-end">
-                                <button type="button" class="btn btn-success px-5 py-3 fw-semibold fs-5" >Make the payment</button>
+                                <button type="button" className="btn btn-success px-5 py-3 fw-semibold fs-5" >Make the payment</button>
                             </div>
                         </div>
                     </div>
-                    <div className="col-4 border rounded bg-light shadow p-4">
-                        <div className="row   ">
-                            <div className="col-12 border p-1 rounded" >
-                                <p className="text-center m-0"> Samsung Galaxy</p>
-                                <p className="text-center m-0 text-danger fw-bold"> 4</p>
-                                <p className="text-center m-0">  $400 </p>
-                            </div>
-                            <div className="col-12 border p-1 rounded mt-2" >
-                                <p className="text-center m-0"> Samsung Galaxy</p>
-                                <p className="text-center m-0 text-danger fw-bold"> 4</p>
-                                <p className="text-center m-0 text-danger fw-bold">  $400 </p>
-                            </div>
-
-                            <div className='text-center mt-4 fs-4'>
-                                Pay <span className='text-danger fw-bold'>$1400</span>
-                            </div>
-                            
-
-                        </div>
+                    <div className="col-xl-4 col-md-3  border border-secondary rounded bg-light shadow p-4">
+                        <OrderDetailsProduct products={productsCart} totalPay={4} />
 
                     </div>
                 </div>
