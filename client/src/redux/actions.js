e8dda4917ea9ce1ba00c4ac00511fc981fc90a2e
emuller1996 <stefano.muller@hotmail.com>
2022-10-22 03:00:15 -0500

    flix(Client/redux) remove redux global state the my cart

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -4,11 +4,7 @@ export const GET_PRODUCTS = "GET_PRODUCTS";
 export const GET_DETAILS = "GET_DETAILS";
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
 
-export const LOAD_PRODUCTS = "LOAD_PRODUCTS";
-export const ADD_TO_CART = "ADD_TO_CART";
-export const DELETE_FROM_CART = "DELETE_FROM_CART";
-export const ADJUST_QUANTITY = "ADJUST_QUANTITY";
-export const LOAD_CURRENT_ITEM = "LOAD_CURRENT_ITEM";
+
 
 
 
@@ -47,53 +43,3 @@ export const getDetails = (id) => {
         }
     }
 }
-
-
-export const loadProducts = (items) => {
-    return {
-      type: LOAD_PRODUCTS,
-      payload: {
-        products: items,  
-      },
-    };
-  };
-  
-  export const addToCart = (event, theItem, itemID) => {
-    event.preventDefault();
-    return {
-      type: ADD_TO_CART,
-      payload: {
-        event: event,
-        item: theItem,
-        id: itemID,  
-      },
-  
-    };
-  };
-
-  export const deleteFromCart = (event, itemID) => {
-    event.preventDefault();
-    return {
-      type: DELETE_FROM_CART,
-      payload: {
-        id: itemID,
-      },
-    };
-  };
-  
-  export const adjustQuantity = (itemID, value) => {
-    return {
-      type: ADJUST_QUANTITY,
-      payload: {
-        id: itemID,
-        qty: value,
-      },
-    };
-  };
-  
-  export const LoadCurrentItem = (item) => {
-    return {
-      type: LOAD_CURRENT_ITEM,
-      payload: item,
-    };
-  };
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,4 +1,4 @@
-import { GET_PRODUCTS, GET_DETAILS, DELETE_PRODUCT, ADD_TO_CART , DELETE_FROM_CART, ADJUST_QUANTITY, LOAD_CURRENT_ITEM } from "./actions";
+import { GET_PRODUCTS, GET_DETAILS, DELETE_PRODUCT } from "./actions";
 
 const stateInitial = {
     products: {},
@@ -24,60 +24,6 @@ export default function rootReducer(state = stateInitial, action) {
                 ...JSON.parse(JSON.stringify(state)),
                 products: state.products.filter((p) => p.id !== action.payload),
             }
-        case ADD_TO_CART:
-            //get the item from products array
-            //const products = action.payload.products;
-            const item = state.products.products.find(
-                (prod) => prod.id === action.payload.id
-            );
-            //
-
-            //console.log(state.cart.item, item.maxQuantity);
-            const checkQty = state.cart.find(
-                (item) => item.qty === action.payload.item.maxQuantity
-            );
-            //console.log(checkQty, 123);
-            //check if the item is in cart already
-            const inCart = state.cart.find((item) =>
-                item.id === action.payload.id ? true : false
-            );
-            
-            return {
-                ...state,
-                cart: inCart
-                    ? state.cart.map((item) =>
-                        item.id === action.payload.id
-                            ? { ...item, qty: item.qty + 1 }
-                            : item
-                    )
-                    : [...state.cart, { ...item, qty: 1 }],
-            };
-        case DELETE_FROM_CART:
-            return {
-                ...state,
-                cart: state.cart.filter((item) => item.id !== action.payload.id),
-            };
-        case ADJUST_QUANTITY:
-            
-            return {
-                ...state,
-                cart: state.cart.map((item) =>
-                    item.id === action.payload.id
-                        ? { ...item, qty: action.payload.qty }
-                        : item
-                ),
-                currentItem:
-                    state.currentItem.id === action.payload.id
-                        ? { ...state.currentItem, qty: action.payload.qty }
-                        : { ...state.currentItem, qty: 1 },
-
-            };
-        case LOAD_CURRENT_ITEM:
-           
-            return {
-                ...state,
-                currentItem: action.payload,
-            };
         default:
             return state;
     }
