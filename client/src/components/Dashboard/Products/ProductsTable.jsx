5aeb782cfaf2c6817cfdec06bd1a0d91de1361d9
emuller1996 <stefano.muller@hotmail.com>
2022-10-23 01:22:06 -0500

    feat (Dashboard/Products) add paginate, delete modal and search product

--- client/src/components/Dashboard/Products/ProductsTable.jsx
+++ client/src/components/Dashboard/Products/ProductsTable.jsx
@@ -1,45 +1,71 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { getAllProducts } from '../../../redux/actions';
 import { useAuth0 } from '@auth0/auth0-react'
 import axios from "axios";
+import ReactPaginate from 'react-paginate';
+import ProductsTableRow from './ProductTableRow';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import SearchBarProducts from '../../CardProductList/SearchBarProducts';
 
-const ProductsTable = () => {
+
+const ProductsTable = ({ itemsPerPage }) => {
 
     const dispatch = useDispatch();
     const products = useSelector(state => state.products.products);
+    const totalPages = useSelector(state => state.products.totalPages);
+    const [size] = useState(12);
+    const [page, setPage] = useState(0);
+    const [search, setSearch] = useState(undefined);
     const { getAccessTokenSilently } = useAuth0()
     const history = useHistory()
+    const [show, setShow] = useState(false);
+    const [id, setId] = useState();
+
+    const handleClose = () => {
+        setShow(false)
+    };
+    const handleShow = () => setShow(true);
 
     useEffect(() => {
-        dispatch(getAllProducts());
-    }, [])
+        dispatch(getAllProducts(size, page,null,null,search));
+    }, [size, page, dispatch,search])
+
+    const handlePageClick = (event) => {
+        setPage(event.selected)
+    };
 
     const deleteP = async (id) => {
-        const token =  await getAccessTokenSilently();
+        const token = await getAccessTokenSilently();
         try {
             await axios.delete(`/product/${id}`, {
                 headers: {
                     Authorization: `Bearer ${token}`
                 }
             })
+            setShow(false)
             //Toast 
-            
+
         } catch (error) {
-          //Toast Error
+            //Toast Error
         }
         dispatch(getAllProducts());
-      }
-
+    }
 
     const updateProduct = (props) => {
-        history.push({pathname:'/create', state: props})
+        history.push({ pathname: '/Dashboard/Products/Update', state: props })
     }
 
     return (
         <>
             <div className="container mt-4 mx-auto bg-light">
+                <div className="row p-2">
+                    <div className="col-8 py-2"><SearchBarProducts setSearch={setSearch} /> </div>
+                    <div className="col-4 py-2"> <Link to={'/Dashboard/Products/Create'} className="btn btn-primary">  <i className="fa-solid fa-cart-plus me-2"></i> Create Product</Link> </div>
+
+                </div>
                 <table className="table">
                     <thead>
                         <tr>
@@ -52,33 +78,52 @@ const ProductsTable = () => {
                     <tbody>
 
                         {products && products.map(p => (
-                            <tr key={p.id}>
-                                <th scope="row">{p.id}</th>
-                                <td>{p.name}</td>
-                                <td>{p.price}</td>
-                                <td>
-                                    <div className="btn-group" role="group" aria-label="Basic example">
-                                        <button type="button" className="btn btn-sm btn-primary" onClick={() => deleteP(p.id)}><i className="fa-solid fa-trash"></i></button>
-                                        <button type="button" className="btn btn-sm btn-warning" onClick={() => updateProduct(
-                                            {
-                                                id: p.id,
-                                                name: p.name,
-                                                image: p.image,
-                                                brand: p.brand ,
-                                                description: p.description,
-                                                price: p.price,
-                                                category: p.category,
-                                                stock: p.stock,
-                                            })
-                                            }><i className="fa-solid fa-pen-to-square"></i></button>   
-                                    </div>
-                                </td>
-                            </tr>
-
+                            <ProductsTableRow key={p.id} p={p} deleteP={deleteP} updateProduct={updateProduct}  setShow={setShow} setId={setId} />
                         ))}
                     </tbody>
+
                 </table>
+                <nav aria-label="navigation">
+                    { totalPages !==0 ?
+                        <ReactPaginate
+                        breakLabel="..."
+                        breakLinkClassName='page-link'
+                        nextLabel=">"
+                        onPageChange={handlePageClick}
+                        pageRangeDisplayed={5}
+                        pageCount={totalPages}
+                        previousLabel="<"
+                        renderOnZeroPageCount={1}
+                        className="pagination justify-content-center"
+                        pageClassName="page-item "
+                        pageLinkClassName="page-link "
+                        activeClassName="active"
+                        previousClassName="page-item"
+                        nextClassName="page-item"
+                        previousLinkClassName="page-link"
+                        nextLinkClassName="page-link"
+                    />  
+                    : ( <> Articulo no Encontrado </>)} 
+                    
+                    </nav>
             </div>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Are you sure?</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>the data is permanently deleted from the database</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="success" onClick={() => deleteP(id)}>
+                        YES
+                    </Button>
+                    <Button variant="danger" onClick={handleClose}>
+                        NO
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            
         </>
     )
 }
