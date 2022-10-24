591732dc8b0eafa2272dd69ede4bc3b515ac2ab4
nahuel <puignahuel.ventas@gmail.com>
2022-10-24 20:20:16 -0300

    fix address show in user order list

--- client/src/components/Profile/Orders/OrderCard.jsx
+++ client/src/components/Profile/Orders/OrderCard.jsx
@@ -24,7 +24,7 @@ const OrderCard = () => {
             });
             setUserOrders(result.data)
         } catch (error) {
-            console.log(error)
+            console.log("getUserOrders Error:", error)
         }
     }
 
@@ -32,10 +32,12 @@ const OrderCard = () => {
         getUserOrders()
       },[])
 
+      console.log(userOrders)
+
     return (
         <div>
             <div className='col-12 mb-3'>
-                { userOrders.length !== 0 ? userOrders.map(order =>
+                { typeof userOrders === 'object' ? userOrders.map(order =>
                     <div key={order.id} className="card border-secondary p-2 shadow-sm mb-3">
                         <div className="row">
                             {/* Order ID */}
@@ -46,21 +48,6 @@ const OrderCard = () => {
                             <div className="col-4 text-danger fs-6 fw-bold">
                                 <p> Order status: {order.status}  </p>
                             </div>
-                            {/* customer information */}
-                            <div className="col-6 mb-3">
-                                <div className=' card position-relative mx-3 border border-danger'>
-                                    <span class="position-absolute mx-5  top-0 start-0 translate-middle badge rounded-pill bg-danger">
-                                    Customer information
-                                    </span>
-                                    <div className="row m-2">
-                                        <div className="col">
-                                            <span className='mr-2'>Andres Patrana</span>
-                                            <span className='ms-2'>{order.user_email}</span>
-                                            <span className='ms-2'> +57 318465156 </span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
                             {/* shipping information */}  
                             <div className="col-6 mb-3">
                                 <div className=' card position-relative mx-3 border border-danger'>
@@ -68,14 +55,7 @@ const OrderCard = () => {
                                         Shipping Information
                                     </span>
                                     <div className="row m-2">
-                                        <div className="col">
-                                            <div className="row">
-                                                <div className="col-6"> <span> {order.shipping_adress} </span></div>
-                                                <div className="col-6"> <span> Valle del Cauca</span></div>
-                                                <div className="col-6"> <span> Cra 43 N 4 a 13</span></div>
-                                                <div className="col-6"> <span> 4891564</span></div>
-                                            </div>
-                                        </div>
+                                            <div className="col-6 mt-1"><span>{order.shipping_address}</span></div>
                                     </div>
                                 </div>
                             </div>
