409b04004a971d68003b8533ccaa44b73be4a1bb
nangonz <nangonz91@gmail.com>
2022-10-26 06:09:01 -0300

    feat(client): useCallback for change status

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,23 +2,25 @@ import CardProductsList from "../components/CardProductList/CardProductsList";
 import Nav from "../components/Nav/Nav";
 import Footer from "../components/Footer/Footer";
 import {  ToastContainer, toast } from 'react-toastify';
-import { useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import { useCallback, useEffect } from "react";
 import axios from "axios";
 
 export default function Home(){
 
   // --------------- temporal
     const location = useLocation();
+    const history  = useHistory();
     const searchParams = new URLSearchParams(location.search);
     const status = searchParams.get('status');
     const id = searchParams.get('id');
 
   // ---------------------------
 
-  useEffect(()=>{
-    if(id) axios.put(`order/status/${status}?id=${id}`);
-  },[id]);
+  useCallback(()=>{
+    if(id && status) axios.put(`order/status/${status}?id=${id}`);
+    history.replace("/")
+  },[]);
 
     return (
       <>
