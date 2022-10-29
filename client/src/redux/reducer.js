2fefcc9a5151b091c5d9ba250fef86bc3206cbe3
emuller1996 <stefano.muller@hotmail.com>
2022-10-28 22:44:50 -0500

    feat(client/redux)add state global total Fav

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,4 +1,4 @@
-import { GET_CATEGORIES, GET_USER, GET_PRODUCTS, GET_DETAILS, DELETE_PRODUCT, ADD_CART , DELETE_CART, CLEAR_CART, GET_ITEMS_LOCAL, SET_TOTAL_PAYMENT, SET_CURRENT_ORDER, SET_PROFILE_IMG} from "./actions";
+import { GET_CATEGORIES, GET_USER, GET_PRODUCTS, GET_DETAILS, DELETE_PRODUCT, ADD_CART, DELETE_CART, CLEAR_CART, GET_ITEMS_LOCAL, SET_TOTAL_PAYMENT, SET_CURRENT_ORDER, SET_PROFILE_IMG, GET_TOTAL_FAV } from "./actions";
 
 const stateInitial = {
     products: {},
@@ -9,7 +9,8 @@ const stateInitial = {
     currentOrder: {},
     profileImg: null,
     user: {},
-    categories: []
+    categories: [],
+    totalFav: 0,
 };
 
 export default function rootReducer(state = stateInitial, action) {
@@ -30,7 +31,7 @@ export default function rootReducer(state = stateInitial, action) {
                 products: state.products.filter((p) => p.id !== action.payload),
             }
         case ADD_CART:
-            return {   
+            return {
                 ...JSON.parse(JSON.stringify(state)),
                 cart: [...JSON.parse(JSON.stringify(state.cart)), action.payload]
             }
@@ -73,6 +74,11 @@ export default function rootReducer(state = stateInitial, action) {
             return {
                 ...JSON.parse(JSON.stringify(state)),
                 categories: action.payload,
+            }
+        case GET_TOTAL_FAV:
+            return {
+                ...JSON.parse(JSON.stringify(state)),
+                totalFav: action.payload,
             }
         default:
             return state;
