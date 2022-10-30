0eb7d97a9420c309a3bfec467da5963a1733b36a
anmarinur <andersonmarindev@gmail.com>
2022-10-30 07:45:57 -0500

    feat: add new star system rate

--- client/src/components/Reviews/AddComment.jsx
+++ client/src/components/Reviews/AddComment.jsx
@@ -1,33 +1,83 @@
-import Star from './Star';
 import Card from 'react-bootstrap/Card'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import { useState } from 'react';
 
-export default function AddComment() {
-
-  const stars = []
-
-  for (let i = 0; i < 5; i++) {
-    stars.push(<Star state={false} size='small'/>)
+export default function AddComment({products}) {
+  const [star, setStar] = useState({
+    star1: false,
+    star2: false,
+    star3: false,
+    star4: false,
+    star5: false
+  })
+  console.log(star)
+  
+  function handleHover(e){
+    console.log(e.target)
   }
 
   return(
     <div>
-      <Card style={{ width: '500px', margin: '30px' }}>
+      <Card style={{margin: '30px' }}>
         <Card.Body>
-          <Card.Title>Add comment</Card.Title>
+          <Card.Title>Comment and rate your product(s)</Card.Title>
           <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Select product</Form.Label>
+              <Form.Control as="select" name="products">
+                {products.map((product) => {
+                  return (
+                  <option key={product.id} value={product.id}>{product.name}</option>
+                  )
+                })}
+              </Form.Control>
+            </Form.Group>
             <Form.Group className="mb-3" controlId="rate">
               <Form.Label>Rate</Form.Label>        
-              <Form.Control as="select" name="rating" onChange={(e) => console.log(e)}>
-                  <option>Select a rate</option>
-                  <option value="0">0</option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-              </Form.Control>
+                <div className="d-flex flex-row">
+                  <i className={star.star1 ? 'fa fa-star text-danger fa-2x' : 'fa fa-star fa-2x'} onClick={() => star.star1 && !star.star2 && !star.star3 && !star.star4 && !star.star5 ? setStar({
+                    star1: false,
+                    star2: false,
+                    star3: false,
+                    star4: false,
+                    star5: false,
+                    }) : setStar({
+                      star1: true,
+                      star2: false,
+                      star3: false,
+                      star4: false,
+                      star5: false,
+                      }) }></i>
+                  <i className={star.star2 ? 'fa fa-star text-danger fa-2x' : 'fa fa-star fa-2x'} onClick={() => setStar({
+                    star1: true,
+                    star2: true,
+                    star3: false,
+                    star4: false,
+                    star5: false,
+                    })}></i>
+                    <i className={star.star3 ? 'fa fa-star text-danger fa-2x' : 'fa fa-star fa-2x'} onClick={() => setStar({
+                    star1: true,
+                    star2: true,
+                    star3: true,
+                    star4: false,
+                    star5: false,
+                    })}></i>
+                    <i className={star.star4 ? 'fa fa-star text-danger fa-2x' : 'fa fa-star fa-2x'} onClick={() => setStar({
+                    star1: true,
+                    star2: true,
+                    star3: true,
+                    star4: true,
+                    star5: false,
+                    })}></i>
+                    <i className={star.star5 ? 'fa fa-star text-danger fa-2x' : 'fa fa-star fa-2x'} onClick={() => setStar({
+                    star1: true,
+                    star2: true,
+                    star3: true,
+                    star4: true,
+                    star5: true,
+                    })}></i>
+                </div>
             </Form.Group>
             <Form.Group className="mb-3" controlId="review">
               <Form.Label>Review:</Form.Label>
