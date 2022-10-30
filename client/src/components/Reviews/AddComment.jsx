6d82e2c6fddb0505ad3c556f79ac9b3c55c32aa4
anmarinur <andersonmarindev@gmail.com>
2022-10-30 10:16:38 -0500

    feat: add inputs with comments and rating

--- client/src/components/Reviews/AddComment.jsx
+++ client/src/components/Reviews/AddComment.jsx
@@ -2,8 +2,9 @@ import Card from 'react-bootstrap/Card'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import { useState } from 'react';
+import Starv2 from './Starv2'
 
-export default function AddComment({products}) {
+export default function AddComment({products, email}) {
   const [star, setStar] = useState({
     star1: false,
     star2: false,
@@ -11,12 +12,36 @@ export default function AddComment({products}) {
     star4: false,
     star5: false
   })
-  console.log(star)
-  
-  function handleHover(e){
-    console.log(e.target)
+
+  const [input, setInput] = useState({
+    email: '',
+    id: '',
+    comment: '',
+    rating: 0
+  })
+  console.log(input)
+
+  function handleChange(e) {
+    console.log('name: ', e.target.name, 'value: ', e.target.value)
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    })
   }
 
+  function onSubmit() {
+    const rating = Object.values(star).filter((el) => el === true).length
+    console.log(rating)
+    setInput({
+      ...input,
+      email,
+      rating
+    })
+
+  }
+
+
+  
   return(
     <div>
       <Card style={{margin: '30px' }}>
@@ -25,67 +50,26 @@ export default function AddComment({products}) {
           <Form>
             <Form.Group className="mb-3">
               <Form.Label>Select product</Form.Label>
-              <Form.Control as="select" name="products">
+              <Form.Control as="select" name="id" onChange={(e) => handleChange(e)}>
+                <option>Select a product</option>
                 {products.map((product) => {
                   return (
-                  <option key={product.id} value={product.id}>{product.name}</option>
+                  <option key={product.id} value={product.id} >{product.name}</option>
                   )
                 })}
               </Form.Control>
             </Form.Group>
             <Form.Group className="mb-3" controlId="rate">
               <Form.Label>Rate</Form.Label>        
-                <div className="d-flex flex-row">
-                  <i className={star.star1 ? 'fa fa-star text-danger fa-2x' : 'fa fa-star fa-2x'} onClick={() => star.star1 && !star.star2 && !star.star3 && !star.star4 && !star.star5 ? setStar({
-                    star1: false,
-                    star2: false,
-                    star3: false,
-                    star4: false,
-                    star5: false,
-                    }) : setStar({
-                      star1: true,
-                      star2: false,
-                      star3: false,
-                      star4: false,
-                      star5: false,
-                      }) }></i>
-                  <i className={star.star2 ? 'fa fa-star text-danger fa-2x' : 'fa fa-star fa-2x'} onClick={() => setStar({
-                    star1: true,
-                    star2: true,
-                    star3: false,
-                    star4: false,
-                    star5: false,
-                    })}></i>
-                    <i className={star.star3 ? 'fa fa-star text-danger fa-2x' : 'fa fa-star fa-2x'} onClick={() => setStar({
-                    star1: true,
-                    star2: true,
-                    star3: true,
-                    star4: false,
-                    star5: false,
-                    })}></i>
-                    <i className={star.star4 ? 'fa fa-star text-danger fa-2x' : 'fa fa-star fa-2x'} onClick={() => setStar({
-                    star1: true,
-                    star2: true,
-                    star3: true,
-                    star4: true,
-                    star5: false,
-                    })}></i>
-                    <i className={star.star5 ? 'fa fa-star text-danger fa-2x' : 'fa fa-star fa-2x'} onClick={() => setStar({
-                    star1: true,
-                    star2: true,
-                    star3: true,
-                    star4: true,
-                    star5: true,
-                    })}></i>
-                </div>
+                <Starv2 star={star} setStar={setStar}/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="review">
               <Form.Label>Review:</Form.Label>
-              <Form.Control as="textarea" rows={2} name="review" onChange={(e) => console.log(e)} placeholder="Enter a review"/>
+              <Form.Control as="textarea" rows={2} name="comment" value={input.comment} onChange={(e) => handleChange(e)} placeholder="Enter a review"/>
             </Form.Group>
           </Form>
           <div className="d-flex justify-content-around py-3 w-50 mx-auto">
-            <Button variant="danger" type="submit"  onClick={(e) => console.log(e)}>Submit</Button>{' '}
+            <Button variant="danger" type="submit"  onClick={onSubmit}>Submit</Button>{' '}
           </div>
 
         </Card.Body>
