723700822394c2d6ee2ad3e7f3fa48b5b50bccd1
anmarinur <andersonmarindev@gmail.com>
2022-11-04 00:57:25 -0500

    feat(Contact.jsx): remove warnings

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -146,18 +146,18 @@ export default function Contact() {
                             </div>  
                         }
                         
-                        <h3 className="fw-bold fs-5 mt-4">E-Mail</h3>
-                        <p className="mb-4 fs-5 text-danger">ecommerce@tecnoshop.com.ar</p>
+                        <h3 className="fw-bold fs-5 mt-5">E-Mail</h3>
+                        <a href="mailto:tecnoshop.henry@gmail.com" style={{textDecoration: 'none'}}><p className="mb-4 fs-5 text-danger">tecnoshop.henry@gmail.com</p></a>
 
-                        <h3 className="fw-bold fs-5 mt-4">Social Media</h3>
+                        <h3 className="fw-bold fs-5 mt-5">Social Media</h3>
                         <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="68" height="68" viewBox="0 0 24 24" stroke-width="1.5" stroke="#a52834" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-instagram" width="68" height="68" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#a52834" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <rect x="4" y="4" width="16" height="16" rx="4" />
                             <circle cx="12" cy="12" r="3" />
                             <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
                             </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="68" height="68" viewBox="0 0 24 24" stroke-width="1.5" stroke="#a52834" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-facebook" width="68" height="68" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#a52834" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
                             </svg>
