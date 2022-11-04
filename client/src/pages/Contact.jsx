b810f3c3068b9dae916834209cca93625c21bccb
anmarinur <andersonmarindev@gmail.com>
2022-11-03 21:52:05 -0500

    feat(Contact.jsx): add map with stores

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,7 +1,17 @@
 import Nav from "../components/Nav/Nav";
 import Footer from "../components/Footer/Footer";
+import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api';
+import { useMemo } from "react";
+import logo from '../components/Nav/images/Logo_60x60.png'
 
 export default function Contact() {
+
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY
+    })
+
+    if (!isLoaded) return <div>Loading...</div>;
+
     return (
         <>
             <Nav />
@@ -21,7 +31,26 @@ export default function Contact() {
                 <p>https://www.instagram.com/tecnoshop.arg/</p>
 
             </div>
+            <Map />
             <Footer />
         </>
     );
+}
+
+function Map() {
+    const center = useMemo(() => ({lat: -34.645164, lng: -58.564632}), [])
+    const mapContainerStyle = {
+        height: "50vh",
+        width: "100%"
+    }
+
+    return (
+        <GoogleMap zoom={12} center={center} mapContainerStyle={mapContainerStyle} clickableIcons={false}>
+            <MarkerF icon={logo} position={{lat: -34.652987, lng: -58.532539}} title = "Technoshop Av. Díaz Velez"/>
+            <MarkerF icon={logo} position={{lat: -34.606529, lng: -58.523029}} title = "Technoshop Av. Lastra"/>
+            <MarkerF icon={logo} position={{lat: -34.626094, lng: -58.738172}} title = "Technoshop Au Acceso Oeste"/>
+            <MarkerF icon={logo} position={{lat: -34.668874, lng: -58.379502}} title = "Technoshop Av. Hipólito Yrigoyen"/>
+            <MarkerF icon={logo} position={{lat: -34.687691, lng: -58.633870}} title = "Technoshop Av. Eva Perón"/>
+        </GoogleMap>
+    )
 }
