8f5e2e89f4d3d11e4a31b8676cfcf4335d2ba412
nangonz <nangonz91@gmail.com>
2022-11-04 00:09:40 -0300

    add goUp navigation button home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -94,11 +94,28 @@ export default function Home() {
     status = '';
   }, []);
 
+  const [ showUp, setShowUp] = useState("0");
+
+  useEffect(()=>{
+    window.onscroll = function(){
+      let scroll = window.scrollY;
+      if(scroll<300) setShowUp("0");
+      if(scroll>=300) setShowUp("1");
+    }
+  }, []);
+
+  const goUp = ()=>{
+    window.scroll(0,0);
+  }
+
   return (
     <>
       <Nav />
       <CardProductsList />
       <Footer />
+      <button style={{position: "fixed", right: 20, bottom: 20, transition: "0.5s" , scale: showUp}} 
+      className={`px-3 py-2 border-0 ms-2 bg-danger text-white rounded mx-5}`} 
+      onClick={goUp}> <i class="fa-solid fa-angle-up"></i> </button>
       <Modal
         show={show}
         onHide={() => setShow(false)}
