161cc96bbb99364488c7008145ef3b1abe18da31
nangonz <nangonz91@gmail.com>
2022-11-04 19:08:16 -0300

    change style position buttons

--- client/src/components/CardProductList/SearchBarProducts.jsx
+++ client/src/components/CardProductList/SearchBarProducts.jsx
@@ -7,8 +7,8 @@ const SearchBarProducts = (props) => {
     return (
         <>
 
-            <div className="row">
-                <div className="col-3 text-center">
+            <div className="row d-flex justify-content-center">
+                <div className="col-3 text-center d-flex align-items-center justify-content-center">
                     <label className="form-check-label fw-semibold" htmlFor="sortByPrice">
                         Sort By Price
                     </label>
@@ -22,16 +22,16 @@ const SearchBarProducts = (props) => {
                     <button className='p-1 border-0 ms-2 bg-danger text-white rounded' onClick={ () => {props.setSortOrder('ASC')} }>  <i className="mx-2 fa-solid fa-arrow-up-wide-short"></i> </button> }
 
                 </div>
-                <div className="col-7 text-center">
+                <div className="col-6 p-0 m-0 text-center ">
 
                     <div className="input-group mx-auto" role="search">
                         <input className="form-control me-0 bg-light" type="search" onChange={(e) => props.setSearch(e.target.value)} placeholder="Search a product" aria-label="Search" />
                     </div>
 
                 </div>
-                <div className="col-2 text-center">
-                    <span className="text-center me-3 ">Show</span>
-                    <select defaultValue={8} onChange={(e) => { props.setPage(e.target.value) }} className="p-1 border-0 w-auto fw-semibold text-white border-0 ms-1 bg-danger text-white rounded" id='page' aria-label="Default select example">
+                <div className="col-3 text-center d-flex align-items-center">
+                    <span className="text-center m-0 fw-semibold">Show</span>
+                    <select defaultValue={12} onChange={(e) => { props.setPage(e.target.value) }} className="d-inline p-1 border-0 w-auto fw-semibold text-white border-0 ms-1 bg-danger text-white rounded" id='page' aria-label="Default select example">
                         <option className='fw-semibold text-white bg-danger' value={4}>4</option>
                         <option className='fw-semibold text-white bg-danger' value={8}>8</option>
                         <option className='fw-semibold text-white bg-danger' value={12}>12</option>
