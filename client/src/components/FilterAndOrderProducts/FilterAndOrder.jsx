9c3a66d2ee101596b8412ca0ba85c5c02de2f1e2
emuller1996 <stefano.muller@hotmail.com>
2022-11-08 00:47:24 -0500

    feat(client/FilterProduct) add style filters.

--- client/src/components/FilterAndOrderProducts/FilterAndOrder.jsx
+++ client/src/components/FilterAndOrderProducts/FilterAndOrder.jsx
@@ -23,98 +23,82 @@ const FilterAndOrder = (props) => {
     }, [props.brands]);
 
     return (
-        <Card className='shadow w-100 p-0 border bg-white' >
-            <Card.Body className=''>
-
-                <div id='filter' className=''>
 
 
-                    <Accordion defaultActiveKey={['0']} alwaysOpen>
-                    <Accordion.Item eventKey="2">
-                            <Accordion.Header>Offers</Accordion.Header>
-                            <Accordion.Body>
+        <div id='filter' className='border shadow rounded '>
+            <Accordion defaultActiveKey={['0']} alwaysOpen>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>Offers</Accordion.Header>
+                    <Accordion.Body>
 
-                                <div className='row mx-1 my-1'>
-                                    <div className="col-xl-12 col-md-6 col-sm-6 col-6 form-check">
-                                        <input checked={props.check} className="form-check-input" type="radio" name="offer" id="allOffer" onChange={(e) => { props.setOffer('') }} />
-                                        <label className="form-check-label fw-semibold" htmlFor="allOffer">
-                                            All
+                        <div className='row mx-1 my-1'>
+                            <div className="col-xl-12 col-md-6 col-sm-6 col-6 form-check">
+                                <input checked={props.check} className="form-check-input" type="radio" name="offer" id="allOffer" onChange={(e) => { props.setOffer('') }} />
+                                <label className="form-check-label fw-semibold" htmlFor="allOffer">
+                                    All
+                                </label>
+                            </div>
+                            {offers && offers.filter(o => o.active === "true").map(o => (
+                                <div key={o.id} className="col-xl-12 col-md-6 col-sm-6 col-6 form-check">
+                                    <input className="form-check-input" type="radio" name="offer" id={o.event} onChange={(e) => { props.setOffer(o.id) }} />
+                                    <label className="form-check-label fw-semibold" htmlFor={o.event}>
+                                        {`${o.event[0].toUpperCase()}${o.event.slice(1)} ${o.discount}%`}
+                                    </label>
+                                </div>
+                            ))}
+                        </div>
+
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Categories</Accordion.Header>
+                    <Accordion.Body>
+                        <div className='row mx-1 my-1'>
+                            <div className="col-xl-12 col-md-6 col-sm-6 col-6 form-check">
+                                <input className="form-check-input" type="radio" name="category" id="all" onChange={(e) => { props.setCategory(0) }} />
+                                <label className="form-check-label fw-semibold" htmlFor="all">
+                                    All
+                                </label>
+                            </div>
+                            {categories.map(element => {
+                                return (
+                                    <div key={element.id} className="col-xl-12 col-md-6 col-sm-6 col-6 form-check">
+                                        <input className="form-check-input" type="radio" name="category" id={element.id} onChange={(e) => { props.setCategory(element.id) }} />
+                                        <label className="form-check-label fw-semibold" htmlFor={element.id}>
+                                            {`${element.category[0].toUpperCase()}${element.category.slice(1)} `}
                                         </label>
                                     </div>
-                                    {offers && offers.filter(o=>o.active==="true").map(o => (
-                                        <div key={o.id} className="col-xl-12 col-md-6 col-sm-6 col-6 form-check">
-                                            <input className="form-check-input" type="radio" name="offer" id={o.event} onChange={(e) => { props.setOffer(o.id) }} />
-                                            <label className="form-check-label fw-semibold" htmlFor={o.event}>
-                                                {`${o.event[0].toUpperCase()}${o.event.slice(1)} ${o.discount}%`}
-                                            </label>
-                                        </div>
-                                    ))}
+                                )
+                            })}
+                        </div>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Brands</Accordion.Header>
+                    <Accordion.Body>
+
+                        <div className='row mx-1 my-1'>
+                            <div className="col-xl-12 col-md-6 col-sm-6 col-6 form-check">
+                                <input checked={props.check} className="form-check-input" type="radio" name="brand" id="allBrands" onChange={(e) => { props.setBrandsSelected('') }} />
+                                <label className="form-check-label fw-semibold" htmlFor="allBrands">
+                                    All
+                                </label>
+                            </div>
+                            {props.brands && props.brands.map(b => (
+                                <div key={b} className="col-xl-12 col-md-6 col-sm-6 col-6 form-check">
+                                    <input className="form-check-input" type="radio" name="brand" id={b} onChange={(e) => { props.setBrandsSelected(e.target.id) }} />
+                                    <label className="form-check-label fw-semibold" htmlFor={b}>
+                                        {b}
+                                    </label>
                                 </div>
-
-                            </Accordion.Body>
-                        </Accordion.Item>
-                        <Accordion.Item eventKey="0">
-                            <Accordion.Header>Categories</Accordion.Header>
-                            <Accordion.Body>
-                                <div className='row mx-1 my-1'>
-                                    <div className="col-xl-12 col-md-6 col-sm-6 col-6 form-check">
-                                        <input className="form-check-input" type="radio" name="category" id="all" onChange={(e) => { props.setCategory(0) }} />
-                                        <label className="form-check-label fw-semibold" htmlFor="all">
-                                            All
-                                        </label>
-                                    </div>
-                                    {categories.map(element => {
-                                        return (
-                                            <div key={element.id} className="col-xl-12 col-md-6 col-sm-6 col-6 form-check">
-                                                <input className="form-check-input" type="radio" name="category" id={element.id} onChange={(e) => { props.setCategory(element.id) }} />
-                                                <label className="form-check-label fw-semibold" htmlFor={element.id}>
-                                                    {`${element.category[0].toUpperCase()}${element.category.slice(1)} `}
-                                                </label>
-                                            </div>
-                                        )
-                                    })}
-                                </div>
-                            </Accordion.Body>
-                        </Accordion.Item>
-                        <Accordion.Item eventKey="1">
-                            <Accordion.Header>Brands</Accordion.Header>
-                            <Accordion.Body>
-
-                                <div className='row mx-1 my-1'>
-                                    <div className="col-xl-12 col-md-6 col-sm-6 col-6 form-check">
-                                        <input checked={props.check} className="form-check-input" type="radio" name="brand" id="allBrands" onChange={(e) => { props.setBrandsSelected('') }} />
-                                        <label className="form-check-label fw-semibold" htmlFor="allBrands">
-                                            All
-                                        </label>
-                                    </div>
-                                    {props.brands && props.brands.map(b => (
-                                        <div key={b} className="col-xl-12 col-md-6 col-sm-6 col-6 form-check">
-                                            <input className="form-check-input" type="radio" name="brand" id={b} onChange={(e) => { props.setBrandsSelected(e.target.id) }} />
-                                            <label className="form-check-label fw-semibold" htmlFor={b}>
-                                                {b}
-                                            </label>
-                                        </div>
-                                    ))}
+                            ))}
 
 
-                                </div>
-                            </Accordion.Body>
-                        </Accordion.Item>
-                    </Accordion>
-
-
-                    
-                </div>
-
-
-                
-
-                
-
-
-
-            </Card.Body>
-        </Card>
+                        </div>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+        </div>
     )
 }
 
