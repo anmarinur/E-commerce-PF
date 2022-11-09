79964a1a7fac3f62752fff0139ec7d41643cb529
nangonz <nangonz91@gmail.com>
2022-11-09 19:53:11 -0300

    fix-charge-DB

--- api/src/utils/chargeProducts.js
+++ api/src/utils/chargeProducts.js
@@ -4,10 +4,10 @@ const { Product, Category } = require('../db.js');
 
 const chargeProducts = () =>{
     try {
-        const categories = ["laptops", "smartphones", "tablets", "smartwatches", "speakers", "tv"];
-        const CategoryDB = categories.map(name=>Category.findOrCreate({where:{category: name}}));
+        const categories = [{id: 1, category: "laptops"}, {id:2, category: "smartphones"},{id:3, category:"tablets"},{id:4, category:"smartwatches"}, {id:5, category: "speakers"}, {id:6, category:"tv"}];
+        const CategoryDB = categories.map(c=>Category.create(c));
         Promise.all(CategoryDB).then(()=>console.log('Categories successfully charged'));
-        const ProductDB = data.map((e) => Product.findOrCreate({where:{
+        const ProductDB= data.map((e) => Product.create({
             name: e.name,
             image: e.image,
             description: e.description,
@@ -16,7 +16,7 @@ const chargeProducts = () =>{
             rating: e.rating,
             stock: e.stock,
             brand: e.brand,
-        }}));
+        }));
         Promise.all(ProductDB).then(()=>console.log('Product successfully charged'));
         
     } catch (error) {
